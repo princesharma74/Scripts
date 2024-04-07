d3b68567c46e81722b9d66582e63ae6e7acfc666
Coder_S_176 <guptajirock176@gmail.com>
2024-04-08 01:16:24 +0530

    no change

--- ProblemSolved.py
+++ ProblemSolved.py
@@ -1,38 +1,49 @@
 import requests
+from collections import Counter
+from datetime import datetime
 
-# GraphQL query
-url = "https://leetcode.com/graphql"
-query_submission = """
-    query RecentAcSubmissions($username: String!, $limit: Int) {
-        recentAcSubmissionList(username: $username, limit: $limit) {
-            id
-            status
+url = 'https://leetcode.com/graphql'
+
+
+def get_problems_solved(username):
+    query_submission = """
+    query getRecentSubmissions($username: String!, $limit: Int) {
+        recentSubmissionList(username: $username, limit: $limit) {
             timestamp
-            url
-            titleSlug
-            title
         }
     }
-"""
+    """
 
-variables = {'username': 'ShivamBedar', 'limit': 1}
+    variables = {'username': username, 'limit': 10}
 
-response = requests.post(
-    url, json={'query': query_submission, 'variables': variables})
+    response = requests.post(
+        url, json={'query': query_submission, 'variables': variables})
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
-    # Extract and process the data from the response
-    ac_submissions = data['data']['recentAcSubmissionList']
-    # Print the extracted data
-    for submission in ac_submissions:
-        print("Title:", submission['title'])
-        print("Title Slug:", submission['titleSlug'])
-        print("Timestamp:", submission['timestamp'])
-        print("url:", submission['url'])
-        print("-" * 50)
-else:
-    # Print an error message if the request was unsuccessful
-    print("Error fetching data. Status code:", response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+        recent_submissions = data['data']['recentSubmissionList']
+
+        # Extracting submission dates
+        submission_dates = [datetime.fromtimestamp(
+            int(sub['timestamp'])).strftime('%Y-%m-%d') for sub in recent_submissions]
+        print(submission_dates)
+        # Counting submissions per month and per day
+        month_count = Counter([date[:7] for date in submission_dates])
+        day_count = Counter(submission_dates)
+
+        # Finding the most active month and day
+        most_active_month = max(month_count, key=month_count.get)
+        most_active_day = max(day_count, key=day_count.get)
+
+        print("Most active month:", most_active_month)
+        print("Number of submissions in the most active month:",
+              month_count[most_active_month])
+        print("Most active day:", most_active_day)
+        print("Number of submissions in the most active day:",
+              day_count[most_active_day])
+
+    else:
+        print("Error fetching data. Status code:", response.status_code)
+
+
+get_problems_solved("ShivamBedar")
