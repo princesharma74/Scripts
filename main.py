43e84f0b91c805018d3b2a9b79f06146e204746e
Prince Sharma <princesharma2899@gmail.com>
2024-06-04 21:55:25 +0530

    Updated time added

--- main.py
+++ main.py
@@ -102,6 +102,7 @@ def run_tasks(users):
                 submissions.extend(user_submissions(platform_id, platform))
                 contest_data.extend(get_contest_history(platform_id, platform))
         # print(json.dumps(contest_data, indent=4))
+        user_data['lastUpdatedAt'] = datetime.now().isoformat()
         push_to_api(f'/users/{email}/update', user_data, method='PATCH') # no pagination required
         # print(contest_data)
         push_to_api(f'/users/{email}/ratingchange/updateAll', contest_data, method='PATCH') # pagination required
